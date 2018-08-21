6dd0f6f5cce65e59008b64acc58143176b585b25
Колесников Владимир Юрьевич <vkolesnikov@amurship.ru>
2018-08-21 13:56:19 +0900

    Initial commit

---
+++ src/main/java/Bot.java
@@ -0,0 +1,89 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bot extends TelegramLongPollingBot {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try{
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiRequestException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public void sendMsg(Message message, String text){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try{
+            setButtons(sendMessage);
+            sendMessage(sendMessage);
+
+        }catch (TelegramApiException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public void onUpdateReceived(Update update) {
+        Model model = new Model();
+        Message message = update.getMessage();
+        if(message !=null && message.hasText()){
+            switch (message.getText()) {
+                case "/help":
+                    sendMsg(message, "Чем могу помочь?");
+                    break;
+                case "/setting":
+                    sendMsg(message, "Что будем настраивать?");
+                    break;
+                default:
+                    try{
+                        sendMsg(message, Weather.getWeather(message.getText(), model));
+                    }
+                    catch (Exception ex){
+                        sendMsg(message, "Город не найден");
+                    }
+
+            }
+        }
+    }
+
+    public void setButtons(SendMessage sendMessage){
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        List<KeyboardRow> keyboardRows = new ArrayList<>();
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+
+        keyboardFirstRow.add(new KeyboardButton("/help"));
+        keyboardFirstRow.add(new KeyboardButton("/settings"));
+
+        keyboardRows.add(keyboardFirstRow);
+        replyKeyboardMarkup.setKeyboard(keyboardRows);
+    }
+
+    public String getBotUsername() {
+        return "WeatherBot";
+    }
+
+    public String getBotToken() {
+         return "679967918:AAHbrwG9Ckr0ABcLrOX2jSfg-r_h_6OfCuQ";
+    }
+}
---
+++ src/main/java/Weather.java
@@ -0,0 +1,40 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Weather {
+    public static String getWeather(String massage, Model model) throws IOException {
+        URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q=" + massage + "&units=metric&appid=8f1a33a55508e0ae4b6de00abd059628");
+
+        Scanner in = new Scanner((InputStream) url.getContent());
+        String result = "";
+        while (in.hasNext()) {
+            result += in.nextLine();
+        }
+
+        JSONObject object = new JSONObject(result);
+        model.setName(object.getString("name"));
+
+        JSONObject main = object.getJSONObject("main");
+        model.setTemp(main.getDouble("temp"));
+        model.setHumidity(main.getDouble("humidity"));
+
+        JSONArray getArray = object.getJSONArray("weather");
+        for(int i = 0; i < getArray.length(); i++){
+            JSONObject obj = getArray.getJSONObject(i);
+            model.setIcon((String)obj.get("icon"));
+            model.setMain((String)obj.get("main"));
+        }
+
+        return "Город: " + model.getName() +"\n" +
+                "Температура: " + model.getTemp() + "C" + "\n" +
+                "Влажность: " + model.getHumidity() + "%" + "\n" +
+                "Описание: " + model.getMain() + "\n" +
+                "http://openweathermap.org/img/w/" + model.getIcon() + ".png";
+    }
+}
